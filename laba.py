561c9e5e0a4481255bd928eef26a5f79370aa972
Zombie1995 <62598313+Zombie1995@users.noreply.github.com>
2021-10-08 13:43:11 +0300

    Add files via upload

---
+++ laba.py
@@ -0,0 +1,36 @@
+import string
+import json
+
+f = open('pokemon_full.json')
+s = f.read()
+jsn = json.loads(s)
+
+#########task1#########
+print('Task1:')
+print(len(s))
+#########task2#########
+print('Task2:')
+print(len(s.translate(s.maketrans(dict.fromkeys(string.punctuation))).replace(' ', '')))
+#########task3#########
+print('Task3:')
+s = ''
+name = ''
+for obj in jsn:
+    if len(obj['description']) > len(s):
+        s = obj['description']
+        name = obj['name']
+print(name)
+#########task4#########
+print('Task4:')
+words_num = 0
+abilities = []
+for obj in jsn:
+    for abil in obj['abilities']:
+        if len(abil.split()) > words_num:
+            words_num = len(abil.split())
+            abilities = []
+        if len(abil.split()) == words_num:
+            abilities.append(abil)
+print(abilities)
+
+f.close()
